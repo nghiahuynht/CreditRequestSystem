194d6dfb9f7680c39433ba60727a20faedeff346
Trong Nghia Huynh <htrongnghia@zuelligpharma.com>
2024-11-16 10:13:19 +0700

    project screen

---
+++ WebApp/Controllers/ProjectController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult ProjectDetail()
+        {
+            return View();
+        }
+
+
+
+    }
+}
