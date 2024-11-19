6b75d6d9563286b63833a8cb2bae94c331cdee37
Trong Nghia Huynh <htrongnghia@zuelligpharma.com>
2024-11-19 16:30:03 +0700

    layout html all screen

---
+++ WebApp/Controllers/ChangeLimitRequestController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class ChangeLimitRequestController : Controller
+    {
+        public IActionResult SearchChangLimitRequest()
+        {
+            return View();
+        }
+
+
+        public IActionResult ChangeLimitRequestDetail()
+        {
+            return View();
+        }
+
+
+    }
+}
